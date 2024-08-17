7f5032ede7d556d0d794a6d74f7562a108eb4f93
AjithChitturi <ajithchitturi84@gmail.com>
2024-08-17 16:44:38 +0530

    deleted .production file

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,9 +1,7 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'myproject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'myproject.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
-
